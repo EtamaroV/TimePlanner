3ef438e861a6cc2d36f80d91f4a04156da0c30a8
Voramate Kittichotirut <voramate05@outlook.co.th>
2024-04-05 15:28:22 +0700

    update metafest and more

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,52 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: { //https://favicon.inbrowser.app/tools/favicon-generator
+        "name": "NSC APP",
+        "short_name": "NSC APP",
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "display": "standalone",
+        "theme_color": "#FFFFFF",
+        "description": "FOR NSC 2024"
+      }
+    })
   ],
   resolve: {
     alias: {
