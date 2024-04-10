741ef231c5217d3e2fc6093d35b8df7db9ee3178
EtamaroV <voramate05@outlook.co.th>
2024-04-10 13:38:18 +0700

    Fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HomeworkView from '@/views/HomeworkView.vue'
 import SettingsView from '@/views/SettingsView.vue'
+import GoalsView from '@/views/GoalsView.vue'
 import Setting1View from '@/views/Setting1View.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
