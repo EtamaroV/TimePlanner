1b9ad2c9a17768786c0d1c564f4863e23e9269df
EtamaroV <voramate05@outlook.co.th>
2024-04-15 23:36:25 +0700

    Update HomeAddtab

--- src/components/Home/HomeAddTab.vue
+++ src/components/Home/HomeAddTab.vue
@@ -2,13 +2,14 @@
 import { RouterLink, RouterView, useRouter} from 'vue-router'
 import { useStore } from 'vuex'
 
-const router = useRouter()
-const store = useStore()
-
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import InputGroup from 'primevue/inputgroup';
+import SelectButton from 'primevue/selectbutton';
+import Listbox from 'primevue/listbox';
 
+const router = useRouter()
+const store = useStore()
 
 </script>
 
@@ -16,16 +17,39 @@ import InputGroup from 'primevue/inputgroup';
     
     <div>
         <div class="allinput-homeadd">
-            <label for="subjectname">วิชา</label>
+
 
             
+            <SelectButton class="subjectexistswitch-homeadd" v-model="subjectExistswitchValue" :options="subjectExistswitch" optionLabel="name" optionValue="value" :allowEmpty="false" aria-labelledby="basic" />
 
-            <InputGroup>
-
-                <InputText :placeholder="store.state.addschedule.subject.name || ''" disabled/>
-                <Button label="เลือกวิชา" @click="router.push({ path: '/add/subject' })" />
+            <label v-if="subjectExistswitchValue == 'EXIST'"  for="subjectname">วิชา</label>
+            <Listbox v-if="subjectExistswitchValue == 'EXIST'" v-model="subjectExistSelect" :options="store.state.SubjectList" optionLabel="name" optionValue="id" listStyle="height:250px" :virtualScrollerOptions="{ itemSize: 38 }"/>
+            
+            <div class="createsubject-homeadd" v-else-if="subjectExistswitchValue == 'CREATE'">
                 
-            </InputGroup>
+                <div class="inputgroup-homeadd">
+                    <label for="subjectname">ชื่อวิชา</label>
+                    <InputText/>
+                </div>
+                
+                <div class="inputgroup-homeadd">
+                    <label for="subjectname">รหัสวิชา</label>
+                    <InputText/>
+                </div>
+
+                <div class="inputgroup-homeadd">
+                    <label for="subjectname">ห้อง</label>
+                    <InputText/>
+                </div>
+
+                <div class="inputgroup-homeadd">
+                    <label for="subjectname">ครูผู้สอน</label>
+                    <InputText/>
+                </div>
+
+            </div>
+
+            <br>
 
             <Button label="TEST" @click="store.commit('addschedule/setSubjectName', 'ทดสอบระบบ');"  />
         </div>
@@ -33,9 +57,57 @@ import InputGroup from 'primevue/inputgroup';
 
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            subjectExistSelect: null,
+
+            subjectExistswitchValue: "CREATE",
+            subjectExistswitch: [
+                {name: 'สร้างวิชาใหม่', value: "CREATE"},
+                {name: 'เลือกวิชาที่มีอยู่', value: "EXIST"},
+            ]
+        }
+    }
+}
+</script>
+
 <style scoped>
 .allinput-homeadd {
     margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 2px;
+}
+.subjectexistswitch-homeadd {
+    margin-bottom: 5px;
+    align-self: center;
+}
+.subjectexistswitch-homeadd .p-button {
+    height: 5px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+
+    opacity: 0;
+        
+}
+
+.createsubject-homeadd {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
+}
+
+.inputgroup-homeadd {
     display: flex;
     flex-direction: column;
     row-gap: 2px;
--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,8 @@ const addschedule = {
             Teacher: null
         }
       },
+      timestart: null,
+      timeend: null,
     },
     mutations: {
         setSubjectName(state, payload) {
@@ -20,6 +22,125 @@ const addschedule = {
     },
 }
 
+const SubjectList = {
+  namespaced: true,
+  state: [
+    {
+      id: 'TEST001',
+      name: 'ทดสอบวิชา1',
+      description: {
+          room: '543',
+          code: 'ท12345',
+          Teacher: 'ครูลองดู'
+      }
+    },
+    {
+      id: 'TEST002',
+      name: 'ทดสอบวิชา2',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST003',
+      name: 'ทดสอบวิชา3',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST004',
+      name: 'ทดสอบวิชา4',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST005',
+      name: 'ทดสอบวิชา5',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST006',
+      name: 'ทดสอบวิชา6',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST007',
+      name: 'ทดสอบวิชา7',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST008',
+      name: 'ทดสอบวิชา8',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST009',
+      name: 'ทดสอบวิชา9',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+    {
+      id: 'TEST010',
+      name: 'ทดสอบวิชา10',
+      description: {
+          room: '345',
+          code: 'ท54321',
+          Teacher: 'ครูลองกอง'
+      }
+    },
+  ]
+}
+
+const ScheduleList = {
+  namespaced: true,
+  state: {
+    sunday: [],
+    monday: [
+      {
+        id: 'TEST001',
+        timestart: 0,
+        timeend: 1
+      },
+    ],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: []
+  },
+  mutations: {
+      setSubjectName(state, payload) {
+          state.subject.name = payload;
+      },
+  },
+}
 
 
 
@@ -35,6 +156,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    addschedule
+    addschedule,
+    SubjectList
   }
 })
